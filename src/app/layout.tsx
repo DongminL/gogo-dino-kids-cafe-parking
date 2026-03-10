bbdef90840c13de92b28286f08c6ccca2de30dd8
DongminL <milkskfk12@gmail.com>
2026-03-10 16:52:20 +0900

    refactor: 불필요한 코드 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "고고 다이노 키즈카페 주차 정산",
   description: "고고 다이노 키즈카페 주차 정산 시스템",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout(
+  { children }: Readonly<{ children: React.ReactNode }>
+) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        suppressHydrationWarning
-      >
+    <html lang="ko">
+      <body className="antialiased" suppressHydrationWarning>
         {children}
       </body>
     </html>
